ea5e1d2b38acc2ae8250333154f6f1d0313de18a
Natasha Brand <tashbrand@outlook.com>
2022-06-04 19:40:42 +1000

    set up server using express.js

---
+++ server.js
@@ -0,0 +1,18 @@
+//import express package
+const express = require('express');
+
+//initialise express framework
+const app = express();
+
+//server static files from public folder - whats a static file..?
+app.use(express.static('public'));
+
+//serve index file for the root "/" path
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/index.html')
+});
+
+//start server, listen for incoming traffic + log message to console
+let server = app.listen(8888, function(){
+    console.log("App server is running on port 8888"); //appears in server side
+});
